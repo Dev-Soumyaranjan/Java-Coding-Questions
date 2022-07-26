6243f986613d5bad7e4a6c3f792bf1328e36c192
Sarthak kumar Bal <balsarthak26@gmail.com>
2022-07-26 22:01:41 +0530

    Added some Changes according to the change request

--- PrepInsta Top 100 Codes/Armstrong_Number.java
+++ PrepInsta Top 100 Codes/Armstrong_Number.java
@@ -1,36 +1,37 @@
 import java.util.Scanner;
-
+/*
+    A number is said to be armstrong number if it is equal to the sum of its own digits raised to the power of the number of digits in the given number.
+ */
 public class Armstrong_Number {
      public static void main(String[] args) {
-        // first we have to give a input number using the Scanner class also we have to import the package of Scanner class
         Scanner sc=new Scanner(System.in);      
         System.out.println("Enter a number");
         int n=sc.nextInt();
-        // we have to store the value of 'n' in a variable 'temp' because with every iteration the value of 'n' will be change.
-        int temp=n; 
-        // we have to declare the a variable 'sum' in which we will store the sum of the cube of each digit of 'n'
+        int temp=n;
+        int temp1=n;
         int sum=0;
-        // we have to declare the variable 'rem' in which we will store the remainder of the division of 'n' by 10
+        int count=0;
         int rem;
-        // now we will start our while loop to get the sum of the cube of each digit of 'n'
         while(n>0)
         {
-            // we have to store the remainder of the division of 'n' by 10 in the variable 'rem'
-            rem=n%10;
-            // we have to store the cube of 'rem' in the variable 'sum'
-            sum=sum+(rem*rem*rem);
-            // we have to decrease the value of 'n' by 1
+            // we have to decrease the value of 'n' by 1 and update the value of count by 1 with each iteration so that we can get the count of number of digits in the given number.
+            count++;
             n=n/10;
         }
-        // now we will check if the value of 'sum' is equal to the value of 'temp' or not
-        if(sum==temp)
+        while(temp>0){
+            // we have to get the remainder of the given number after dividing it by 10.
+            rem=temp%10;
+            //now we will use the power function to get the value of the given number raised to the power of the count of number of digits in the given number and add it to the sum.
+            sum=sum+(int)(Math.pow(rem,count));
+            temp=temp/10;
+        }
+        // now we will check if the value of 'sum' is equal to the value of 'temp1' or not , if it is equal then it is an armstrong number otherwise it is not.
+        if(sum==temp1)
         {
-            // if the value of 'sum' is equal to the value of 'temp' then we will print the message
             System.out.println("The number is an Armstrong number");
         }
         else
         {
-            // if the value of 'sum' is not equal to the value of 'temp' then we will print the message
             System.out.println("The number is not an Armstrong number");
         }
      }   
